e5720043a0a0137b108297e226c3709bf54da878
Hamdi Helmi <helmihamdi977@gmail.com>
2025-12-01 11:05:53 +0100

    okitotos

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuration de base de l'API
 const API = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://bambou-glow-up-test.onrender.com/api",
 });
 
 
